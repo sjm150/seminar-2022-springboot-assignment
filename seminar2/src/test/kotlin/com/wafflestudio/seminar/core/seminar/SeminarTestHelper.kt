a07df81bda145dc814b5aef8e55149d5280a89ce
Jungmin <sjm150@snu.ac.kr>
2022-11-12 02:47:35 +0900

    Add tests for seminar service

--- seminar2/src/test/kotlin/com/wafflestudio/seminar/core/seminar/SeminarTestHelper.kt
+++ seminar2/src/test/kotlin/com/wafflestudio/seminar/core/seminar/SeminarTestHelper.kt
@@ -1,13 +1,20 @@
 package com.wafflestudio.seminar.core.seminar
 
+import com.wafflestudio.seminar.core.maptable.SeminarUser
+import com.wafflestudio.seminar.core.maptable.SeminarUserRepository
 import com.wafflestudio.seminar.core.seminar.database.SeminarEntity
 import com.wafflestudio.seminar.core.seminar.database.SeminarRepository
+import com.wafflestudio.seminar.core.user.api.request.Role
+import com.wafflestudio.seminar.core.user.database.UserEntity
+import com.wafflestudio.seminar.core.user.database.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 internal class SeminarTestHelper @Autowired constructor(
-    private val seminarRepository: SeminarRepository
+    private val userRepository: UserRepository,
+    private val seminarRepository: SeminarRepository,
+    private val seminarUserRepository: SeminarUserRepository,
 ) {
     fun createSeminar(
         name: String = "",
@@ -15,8 +22,9 @@ internal class SeminarTestHelper @Autowired constructor(
         count : Int = 1,
         time : String = "00:00",
         online : Boolean = true,
+        instructor: UserEntity,
     ): SeminarEntity {
-        return seminarRepository.save(
+        val seminar =  seminarRepository.save(
             SeminarEntity(
                 name,
                 capacity,
@@ -25,5 +33,20 @@ internal class SeminarTestHelper @Autowired constructor(
                 online,
             )
         )
+        
+        val seminarUser = seminarUserRepository.save(
+            SeminarUser(
+                seminar,
+                instructor,
+                Role.Instructors
+            )
+        )
+        
+        instructor.seminarUser.add(seminarUser)
+        seminar.seminarUser.add(seminarUser)
+        userRepository.save(instructor)
+        seminarRepository.save(seminar)
+        
+        return seminar
     }
 }
